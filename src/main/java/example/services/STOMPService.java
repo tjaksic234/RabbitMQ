b7be12989d163c9b6ec46a60422b653b942add25
tjaksic234 <jaksa.teo@gmail.com>
2024-10-08 08:02:16 +0200

    removed some unnecessary code

--- src/main/java/example/services/STOMPService.java
+++ src/main/java/example/services/STOMPService.java
@@ -5,11 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
-@Transactional
 @Slf4j
 public class STOMPService {
 
